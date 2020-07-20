c8272d130c02ff560f20fffe85008d5f23bd9651
Glitch (jlbubbles092-hexx-backup) <none>
2020-07-20 15:13:10 +0000

    🐘💁 Checkpoint ./commands/fun/meme.js:15553712/1985

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -12,20 +12,22 @@ module.exports = class MemeCommand extends Command {
 		});
 	}
 
-async run(bot, message, nsfw) {
-  const { body } = await snekfetch
-    .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-    .query({ limit: 800 });
-  const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-  if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-  const randomnumber = Math.floor(Math.random() * allowed.length)
-  const embed = new Discord.RichEmbed()
-  embed.setColor(`#ff0000`)
-  embed.setTitle(allowed[randomnumber].data.title)
-  embed.setDescription("Posted by: " + allowed[randomnumber].data.author)
-  embed.setImage(allowed[randomnumber].data.url)
-  embed.addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-  embed.setFooter("Memes provided by r/dankmemes")
-  message.channel.send(embed)
+async run(message, args) {
+    const member = args.member;
+    const channel = message.channel
+    const { body } = await snekfetch
+            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.MessageEmbed()
+        embed.setColor('#ff0000')
+        embed.setTitle(allowed[randomnumber].data.title)
+        embed.setDescription("Posted by: " + allowed[randomnumber].data.author)
+        embed.setImage(allowed[randomnumber].data.url)
+        embed.addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        embed.setFooter("Memes provided by r/dankmemes");
+        message.channel.send(embed)
   }
 };
