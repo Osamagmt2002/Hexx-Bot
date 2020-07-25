e1f75b4f3e540abc7b818ccd07c92af651e8d318
Glitch (jlbubbles092-hexx-backup) <none>
2020-07-25 16:03:59 +0000

    👲🚃 Checkpoint ./commands/admin/purge.js:15553712/70

--- commands/admin/purge.js
+++ commands/admin/purge.js
@@ -26,7 +26,5 @@ module.exports = class KickCommand extends Command {
     await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
         msg.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
     )});
-    
-    msg.channel.send(`Successfully deleted ${amount} messages.`)
   }
 }
