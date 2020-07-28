b0d07f05a0ba42b479aed21b01c702bc217d237d
Glitch (jlbubbles092-hexx-backup) <none>
2020-07-28 11:59:15 +0000

    📼🚈 Checkpoint ./server.js:15553712/446

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ client.registry
 
 client.on('ready', () => {
     console.log(`Logged in to ${client.user.tag}!`);
-    client.user.setActivity(`Serving ${client.guilds.cache.size} servers.`);
+    client.user.setActivity(`serving ${client.guilds.cache.size} servers.`);
 });
 
 const db = require("quick.db") //using quick.db package
@@ -33,27 +33,13 @@ const db = require("quick.db") //using quick.db package
 client.on("guildCreate", (guild) => {
   // This event triggers when the bot joins a guild
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers.`);
-  
-  let channelID;
-    let channels = guild.channels;
-    channelLoop:
-    for (let c of channels) {
-        let channelType = c[1].type;
-        if (channelType === "text") {
-            channelID = c[0];
-            break channelLoop;
-        }
-    }
-
-    let channel = client.channels.get(guild.systemChannelID || channelID);
-    channel.send(`Hello! Don't forget to make a voice channel called **Record** when recording your voice!`);
+  client.user.setActivity(`serving ${client.guilds.cache.size} servers.`);
 });
 
 client.on("guildDelete", (guild) => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers.`);
+  client.user.setActivity(`serving ${client.guilds.cache.size} servers.`);
 });
 
 client.on("guildMemberAdd", (member) => { //usage of welcome event
